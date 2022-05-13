636d8025688cf815d71cbce5c46e8a43f021ad65
Manuel Servin <55255832+Manenpren@users.noreply.github.com>
2022-05-13 18:30:26 -0500

    Create sort.py
    Create a main file

---
+++ sort.py
@@ -0,0 +1,27 @@
+from numpy import record
+import requests  
+from queue01 import QueueService
+
+print("Reading data...")
+URL = 'https://jsonplaceholder.typicode.com/posts' 
+
+data = requests.get(URL) 
+
+data = data.json() 
+
+response = {}
+
+for element in data: 
+    if not response.get(element['userId']): 
+        response[element['userId']] =  { 'records':[]}
+    else:
+        element_replace = response[element['userId']]['records']
+        element_replace.append(element)
+
+queue_service = QueueService.create()
+
+response_queue = queue_service.createCommunity(response)
+
+print(f'Response Queue: {response_queue}')
+
+
